1db59a148d681e647de96ab6856ce2860108af6f
Szymon91 <Szymon91@poczta.onet.pl>
2019-06-04 11:03:56 +0200

    hamburger menu

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const menuBtn = document.querySelector('ul>li');
 const menuItems = document.querySelector('ul>li>ol');
+const toggle = document.querySelector('.toggle');
+const ul = document.querySelector('#header__container ul');
 
 menuBtn.addEventListener("click", () => {
     if (menuItems.style.display === 'none') {
         menuItems.style.display = 'block';
     } else {
         menuItems.style.display = 'none';
+    }
+});
+
+toggle.addEventListener("click", () => {
+    if (ul.className == 'clearfix') {
+        ul.className = 'open clearfix';
+    } else {
+        ul.className = 'clearfix';
     }
 });
